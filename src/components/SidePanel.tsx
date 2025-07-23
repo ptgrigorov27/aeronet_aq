e976e6ae89294319a93f63ee0efae57425e15d65
Petar T Grigorov <pgrigoro@gs618-aerol1.gsfc.nasa.gov>
2025-07-23 09:12:24 -0400

    modified:   src/components/SidePanel.tsx

--- src/components/SidePanel.tsx
+++ src/components/SidePanel.tsx
@@ -299,17 +299,15 @@ const SidePanel: React.FC = (setExType) => {
       d = new Date();
       nearestDate(d)
         .then((res) => {
-          console.log("nearestDate resolved to:", res);
           d = new Date(res);
         })
         .catch((error) => {
-          console.error("nearestDate error:", error);
+          console.error(error);
         });
     } else {
       d = new Date(inDate);
     }
     d.setUTCMinutes(0);
-    console.log("initUTCDate returning:", d.toISOString());
     return d;
   }
 
