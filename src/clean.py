317d5531cb65cdec77d52bf26a607e47936401a1
Petar Todorov Grigorov <98364934+ptgrigorov27@users.noreply.github.com>
2025-09-30 16:45:46 -0400

    Add files via upload

--- src/clean.py
+++ src/clean.py
@@ -1,9 +1,14 @@
 import pandas as pd
 
-desired_cols = ["sitename", "Latitude", "Longitude"]
+desired_cols = ["sitename", "Latitude", "Longitude", "Forecast"]
 df = pd.read_csv("./station_270_updates_Feb_11_2025_updated.csv", encoding="latin-1")
 df2 = pd.read_csv("./stations_AERONET_07262024.csv", encoding="latin-1")
 df3 = pd.read_csv("./stations_OpenAQ_05082025.csv", encoding="latin-1")
+
+df['Forecast'] = "DoS Missions"
+df2['Forecast'] = "AERONET"
+df3['Forecast'] = "Open AQ"
+
 df = pd.concat([df[desired_cols], df2[desired_cols], df3[desired_cols]], axis=0)
 
 for col in df.columns:
