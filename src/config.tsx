850d31ac26e156433c2d67bb43428b1af3194d55
Petar Todorov Grigorov <98364934+ptgrigorov27@users.noreply.github.com>
2025-10-14 13:20:06 -0400

    Add files via upload
    config file exports API link to Africa AQ data

--- src/config.tsx
+++ src/config.tsx
@@ -4,3 +4,5 @@ export const API_ARNT =
   "https://aeronet.gsfc.nasa.gov/cgi-bin/web_print_air_quality_index_AERONET?";
 export const API_AQ =
   "https://aeronet.gsfc.nasa.gov/cgi-bin/web_print_air_quality_index_OpenAQ?";
+export const API_AAQE =
+  "https://aeronet.gsfc.nasa.gov/cgi-bin/web_print_air_quality_index_AFRICA?";
