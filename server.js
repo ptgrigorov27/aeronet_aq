511c5a7e7ceb2732770fa3cb20ff23ca777971b0
Petar Todorov Grigorov <98364934+ptgrigorov27@users.noreply.github.com>
2025-12-17 14:24:55 -0500

    Update server.js
    Replace /api with different endpoint, because /api has active rewrite rule on host level.

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import cors from "cors";
 
 const app = express();
-const PORT = 8081; // matching port as nginx
+const PORT = 4000;
 
 app.use(
   cors({
@@ -11,18 +11,9 @@ app.use(
   })
 );
 
-app.use("/api/openaq", (req, res, next) => {
-  // Force any "manviewer" in the URL path to "aqforecast"
-  if (req.url.includes("manviewer")) {
-    req.url = req.url.replace(/manviewer/g, "aqforecast");
-    console.log(`[PATCH] Rewriting URL to ${req.url}`);
-  }
-  next();
-});
-
 const OPENAQ_KEY = process.env.VITE_OPENAQ_API_KEY;
 
-app.get("/api/openaq/locations", async (req, res) => {
+app.get("/aqi/openaq/locations", async (req, res) => {
   try {
     const { page = 1, limit = 100 } = req.query;
 
